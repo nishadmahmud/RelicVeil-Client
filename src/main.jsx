03f7e6bbeb552d67fcf224807c168e1f9785fb0c
Nishad <mahmudnishad253@gmail.com>
2025-06-13 23:44:18 +0600

    fix deploy issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import router from './routes/Routes'
+import router from './routes/routes'
 import AuthProvider from './auth/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
