44cb76e125fb0a1a0f9bc309eae31b3bf170ce86
Nishad <mahmudnishad253@gmail.com>
2025-06-13 23:40:41 +0600

    Still trying to remove helmet

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './routes/Routes'
 import AuthProvider from './auth/AuthProvider'
-import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
-    </HelmetProvider>
   </React.StrictMode>,
 )
